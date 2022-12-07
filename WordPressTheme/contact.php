0cca0d0e2de56152cd27ab8ab040aff914e75147
Yoshiaki Numajiri <afmaar128@gmail.com>
2022-12-07 12:04:48 +0900

    お問い合わせセクションをパーツ化

---
+++ WordPressTheme/contact.php
@@ -0,0 +1,12 @@
+<section class="contact" id="contact">
+  <div class="contact__l-inner l-inner">
+    <div class="contact__section-titles section-titles">
+      <p class="section-titles__section-subtitle">Contact</p>
+      <h2 class="section-titles__section-title section-titles__section-title--contact">お問い合わせ</h2>
+    </div>
+    <p class="contact__descript">
+      テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
+    </p>
+    <a href="/contact/" class="btn contact__btn">お問い合わせへ</a>
+  </div>
+</section>
--- WordPressTheme/front-page.php
+++ WordPressTheme/front-page.php
@@ -238,19 +238,7 @@
   </div><!-- /.blog__inner l-inner -->
 </section><!-- /.blog -->
 
-<section class="contact" id="contact">
-  <div class="contact__l-inner l-inner">
-    <div class="contact__section-titles section-titles">
-      <p class="section-titles__section-subtitle">Contact</p>
-      <h2 class="section-titles__section-title section-titles__section-title--contact">お問い合わせ</h2>
-    </div>
-    <p class="contact__descript">
-      テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
-    </p>
-    <a href="/contact/" class="btn contact__btn">お問い合わせへ</a>
-  </div>
-</section>
-
+<?php get_template_part('contact'); ?>
 
 
 <?php get_footer(); ?>
